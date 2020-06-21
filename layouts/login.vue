874146a6beed4dbf28a8102530bebe919edc5c32
cutebron <u22pmb11o5a@gmail.com>
2020-06-22 00:18:51 +0800

    #develop 登入頁面layout

---
+++ layouts/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app class="blue">
+    <Nuxt/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'loginLayout',
+
+}
+</script>
+
+<style scoped>
+.blue{
+  background: #4267b2;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,74 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-  1234
-  </v-layout>
+  <v-row justify="center" align="center">
+      <v-card  class="mycard mx-lg-auto mx-auto" max-width="344">
+        <v-img
+          height="250"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png">
+        </v-img>
+
+       <v-card-title  class="d-flex justify-center align-center">
+          <h2>隨機聊天室</h2>
+        </v-card-title >
+        <v-card-text class="d-flex justify-center">
+          <v-form
+            ref="form"
+            v-model="isValid"
+            lazy-validation
+            @submit.prevent="submit"
+          >
+            <v-text-field
+              v-model="user.name"
+              :counter="10"
+              :rules="nameRules"
+              label="Name"
+              class="field"
+              required
+            />
+            <v-btn
+              :disabled="!isValid"
+              color="primary"
+              class="mt-3 mx-12"
+              type="submit"
+            >
+              開始聊天
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+  </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import LoginWindow from '~/components/LoginWindow.vue'
+
 
 export default {
+  name: "Home",
+  layout: "login",
   components: {
-    Logo,
-    VuetifyLogo
-  }
+
+  },
+  data:()=>({
+    isValid: true,
+    user: {
+      name: "",
+      typingStatus: false,
+    },
+  })
+
+
 }
 </script>
+<style scoped>
+.mycard{
+  border-radius:5%;
+  background-color:#f7eddc;
+}
+h2{
+  color:#424242
+}
+.field .v-input{
+  color:#686363!important;
+
+}
+</style>
